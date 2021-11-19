c9f77feabacec89c8468b0e241e92cb8c06f9169
julianguacaneme <julianguacaneme@gmail.com>
2021-11-19 14:54:43 -0500

    Update ES8.js

--- ES8.js
+++ ES8.js
@@ -34,12 +34,12 @@ const data = {
 //ASYNC AWAIT
 
 const helloWorld = () => {
-    return new Promise((resolve, reject) =>
+    return new Promise((resolve, reject) => {
     (true)
     ? setTimeout(()=> resolve("Hello World"), 300)
     : reject(new Error("Test error"))
+})
 };
-
 const helloAsync = async () => {
     const hello = await helloWorld(),
     console.log(hello);
