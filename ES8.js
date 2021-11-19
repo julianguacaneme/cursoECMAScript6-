affaa101a7d02028c713808817bf819ec79bd7be
julianguacaneme <julianguacaneme@gmail.com>
2021-11-19 14:54:20 -0500

    Update ES8.js

--- ES8.js
+++ ES8.js
@@ -30,3 +30,20 @@ console.log(string.padEnd(12, '-------'));
 const data = {
     fontend: "oscar",
 };
+
+//ASYNC AWAIT
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) =>
+    (true)
+    ? setTimeout(()=> resolve("Hello World"), 300)
+    : reject(new Error("Test error"))
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld(),
+    console.log(hello);
+};
+
+helloAsync();
+
