e53bba0e4e216805afc619f0517412de160fc66b
julianguacaneme <julianguacaneme@gmail.com>
2021-11-25 10:04:13 -0500

    ES12

--- src/ES11/ES11.js
+++ src/ES11/ES11.js
@@ -2,8 +2,41 @@
 
 const button = document.getElementById("btn");
 
-button.addEventListener('click', async function() {
+button.addEventListener("click", async function() {
     const module = await
-    import ("./file.js")
+    import ("./file.js");
     module.hello();
 });
+
+//BigInt()
+
+//Promise allSettled
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => rerolve("resolve1"));
+//
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+//global this
+
+console.log(window);
+console.log(globalThis);
+
+//Operador null
+
+const fooo = null ? ? 'default string';
+console.log(fooo);
+
+
+// ⛓ Optional chaining
+
+const user = {};
+console.log(user ? .profile ? .email);
+
+if (user ? .profile ? .email) {
+    console.log('email')
+} else {
+    console.log('fail')
+}
---
+++ src/ES12/ES12.js
@@ -0,0 +1,33 @@
+//replace all
+
+const string =
+    "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web";
+
+const replaceString = string.replace('JavaScript', 'Python');
+console.log(replaceString); // solo remplazaba el primer elemento
+
+const replaceString2 = string.replaceAll('JavaScript', 'Python');
+console.log(replaceString2);
+
+//METODOS PRIVADOS
+
+class Message {#
+    show(val) {
+        console.log(val);
+    };
+    get# add(val) {
+
+    }
+}
+
+const message = new Message();
+message.show('Hola!')
+
+//PROMISE ANY
+
+const promise1 = new Promise((resolve, reject) => reject("1"));
+const promise2 = new Promise((resolve, reject) => resolve("2"));
+const promise3 = new Promise((resolve, reject) => resolve("3"));
+
+Promise.any([promise1, promise2, promise3])
+    .then(response => console.log(response));
